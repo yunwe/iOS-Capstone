9063a2cb6c39dd8126a129ae27427a6e3bc2bddf
yunwe <sawyunwe.ucsy@gmail.com>
2024-08-21 17:08:13 +0700

    init project

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Saw Yu Nwe on 21/8/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
